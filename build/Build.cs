b098afd1c43601f04c2d729b4da81d690d829c24
Georg Dangl <georg@dangl.me>
2018-09-14 20:14:26 +0200

    Configure build

--- build/Build.cs
+++ build/Build.cs
@@ -1,37 +1,98 @@
-using System;
-using System.Linq;
+using Nuke.Azure.KeyVault;
 using Nuke.Common;
-using Nuke.Common.ProjectModel;
-using static Nuke.Common.EnvironmentInfo;
+using Nuke.Common.Git;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.GitHub;
+using System;
+using System.IO.Compression;
+using System.Linq;
+using static Nuke.Common.ChangeLog.ChangelogTasks;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.GitHub.GitHubTasks;
 
 class Build : NukeBuild
 {
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main() => Execute<Build>(x => x.Compile);
 
-    [Solution] readonly Solution Solution;
+    [KeyVaultSettings(
+        BaseUrlParameterName = nameof(KeyVaultBaseUrl),
+        ClientIdParameterName = nameof(KeyVaultClientId),
+        ClientSecretParameterName = nameof(KeyVaultClientSecret))]
+    readonly KeyVaultSettings KeyVaultSettings;
+
+    [Parameter] readonly string KeyVaultBaseUrl;
+    [Parameter] readonly string KeyVaultClientId;
+    [Parameter] readonly string KeyVaultClientSecret;
+    [GitVersion] readonly GitVersion GitVersion;
+    [GitRepository] readonly GitRepository GitRepository;
+
+    [KeyVaultSecret] readonly string GitHubAuthenticationToken;
+
+    string ChangeLogFile => RootDirectory / "CHANGELOG.md";
 
     Target Clean => _ => _
         .Executes(() =>
         {
+            DeleteDirectories(GlobDirectories(SourceDirectory, "**/bin", "**/obj"));
+            EnsureCleanDirectory(OutputDirectory);
         });
 
     Target Restore => _ => _
         .DependsOn(Clean)
         .Executes(() =>
         {
+            DotNetRestore(s => DefaultDotNetRestore);
         });
 
     Target Compile => _ => _
         .DependsOn(Restore)
         .Executes(() =>
         {
+            DotNetBuild(s => DefaultDotNetBuild
+                .SetFileVersion(GitVersion.GetNormalizedFileVersion())
+                .SetAssemblyVersion(GitVersion.AssemblySemVer));
         });
 
-    private Target Pack => _ => _
+    Target Publish => _ => _
         .DependsOn(Compile)
         .Executes(() =>
         {
+            DotNetPublish(s => s
+                .SetProject(SourceDirectory / "AzureFolderMigrator")
+                .SetOutput(OutputDirectory)
+                .SetConfiguration(Configuration));
+        });
+
+    Target PublishGitHubRelease => _ => _
+        .DependsOn(Publish)
+        .Requires(() => GitHubAuthenticationToken)
+        .Executes(async () =>
+        {
+            var releaseTag = $"v{GitVersion.NuGetVersion}";
+
+            var changeLogSectionEntries = ExtractChangelogSectionNotes(ChangeLogFile);
+            var latestChangeLog = changeLogSectionEntries
+                .Aggregate((c, n) => c + Environment.NewLine + n);
+            var completeChangeLog = $"## {releaseTag}" + Environment.NewLine + latestChangeLog;
+
+            var repositoryInfo = GetGitHubRepositoryInfo(GitRepository);
+
+            var zipPath = OutputDirectory / "AzureFolderMigrator.zip";
+            ZipFile.CreateFromDirectory(OutputDirectory, zipPath);
+
+            var isStable = GitVersion.BranchName.Equals("master") || GitVersion.BranchName.Equals("origin/master");
+
+            await PublishRelease(x => x
+                    .SetArtifactPaths(new string[] { zipPath })
+                    .SetReleaseNotes(completeChangeLog)
+                    .SetCommitSha(GitVersion.Sha)
+                    .SetPrerelease(!isStable)
+                    .SetRepositoryName(repositoryInfo.repositoryName)
+                    .SetRepositoryOwner(repositoryInfo.gitHubOwner)
+                    .SetTag(releaseTag)
+                    .SetToken(GitHubAuthenticationToken));
         });
 }
